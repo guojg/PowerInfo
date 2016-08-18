55c9d65908e4d5d5adde02cc63e8420cf048f1f2
guojianguang.kursk <15900295806@163.com>
2016-08-18 17:43:53 +0800

    Signed-off-by: guojianguang.kursk <15900295806@163.com>

--- PowerInfo/src/com/github/totalquantity/totaldata/controller/TotalDataController.java
+++ PowerInfo/src/com/github/totalquantity/totaldata/controller/TotalDataController.java
@@ -2,6 +2,7 @@ package com.github.totalquantity.totaldata.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.basicData.model.BasicYear;
 import com.github.basicData.service.BasicDataService;
 import com.github.totalquantity.totaldata.service.TotalDataService;
 
@@ -100,5 +102,46 @@ public class TotalDataController {
 		return resultJson;
 	}
 	
+	@RequestMapping(value = "/getyears",produces="application/json;charset=UTF-8")
+	public @ResponseBody List<BasicYear> getYears(HttpServletRequest request) {
+		try {
+			 String baseyear = request.getParameter("baseyear")==null?"2015": request.getParameter("baseyear");
 
+			 String planyear = request.getParameter("planyear")==null?"2020": request.getParameter("planyear");
+
+			JSONObject obj = new JSONObject();
+			obj.put("baseyear", baseyear);
+			obj.put("planyear", planyear);
+			return totalDataService.getYears(obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+
+	@RequestMapping(value = "/queryData6" ,produces="text/plain;charset=UTF-8")
+	public @ResponseBody String queryData6(HttpServletRequest request,
+			HttpServletResponse response) {
+		//response.setCharacterEncoding("UTF-8");
+		// String jsonParam = request.getParameter("jsonParam");
+		 String taskid = request.getParameter("taskid");
+		 String year = request.getParameter("year")==null?"": request.getParameter("year");
+
+		 String baseyear = request.getParameter("baseyearInt")==null?"2015": request.getParameter("baseyearInt");
+		 String algorithm = request.getParameter("algorithm")==null?"2015": request.getParameter("algorithm");
+
+		JSONObject obj = new JSONObject();
+		obj.put("year", year);
+		obj.put("baseyearInt", Integer.parseInt(baseyear));
+		obj.put("taskid",taskid);
+		obj.put("algorithm",algorithm);
+		/*obj.put("year", "2005,2006,2007,2015,2020");
+		obj.put("baseyearInt", 2015);
+		obj.put("taskid",11);
+		obj.put("algorithm",5);*/
+		String resultJson = totalDataService.queryData6(obj);
+	
+		return resultJson;
+	}
 }
